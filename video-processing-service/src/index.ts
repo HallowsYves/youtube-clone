820945f693ebb6cfcf35899a9b81e40faa985e72
HallowsYves <vyves246@gmail.com>
2026-01-08 13:32:35 -0800

    ffmpeg working for 360p

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,33 @@
 import express, { response } from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-// .get has a request, and it has a response
-app.get("/", (request, response) => {
-    response.send("Hello World!")
+app.post('/process-video', (request, response) => {
+    const inputFilePath = request.body.inputFilePath;
+    const outputFilePath = request.body.outputFilePath;
+
+    // Check if file path is valid
+    if (!inputFilePath || !outputFilePath) {
+        return response.status(400).send('Bad Request: Missing file path');
+    }
+
+    // Ffmpeg command
+    ffmpeg(inputFilePath)
+      .outputOptions('-vf', 'scale=-1:360')
+      .on('end', function() {
+        console.log('Processing finished successfully');
+        response.status(200).send('Processing finished successfully');
+      })
+      .on('error', function(err:any) {
+        console.log("An error occured: " + err.message);
+        response.status(500).send('An error occured: ' + err.message);
+      })
+      .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`video processing servicel listening at http://localhost:${port}`);
+    console.log(`Server is listening on port: ${port}`);
 });
